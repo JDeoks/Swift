30925840e43abc3c10817aed098a4f44b9c9bc8e
JDeoks <duginee@naver.com>
2023-12-08 07:34:35 +0900

    Fetures/Chat 0-4강

--- Features/Chat/Chat/ViewControllers/SignUpViewController.swift
+++ Features/Chat/Chat/ViewControllers/SignUpViewController.swift
@@ -6,17 +6,24 @@
 //
 
 import UIKit
+import PhotosUI
 import RxSwift
 import RxCocoa
+import RxGesture
 import FirebaseAuth
 import FirebaseFirestore
+import FirebaseStorage
 
 class SignUpViewController: UIViewController {
     
+    var profileImage: UIImage! = nil
     let db = Firestore.firestore()
+    let storage = Storage.storage()
     
+    let didFinishPickingDone = PublishSubject<Void>()
     let disposeBag = DisposeBag()
     
+    @IBOutlet var imageSelectButtonImageView: UIImageView!
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var passwordTextView: UITextField!
@@ -25,7 +32,12 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initUI()
         action()
+        bind()
+    }
+    func initUI() {
+
     }
     
     func action() {
@@ -40,13 +52,40 @@ class SignUpViewController: UIViewController {
                 self.dismiss(animated: true)
             }
             .disposed(by: disposeBag)
-    }
         
+        imageSelectButtonImageView.rx.tapGesture()
+            .when(.recognized)
+            .subscribe(onNext: { _ in
+                self.presentPicker()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func bind() {
+        didFinishPickingDone
+            .subscribe { _ in
+                DispatchQueue.main.async {
+                    self.imageSelectButtonImageView.contentMode = .scaleAspectFill
+                    self.imageSelectButtonImageView.image = self.profileImage
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    /// 사진,  이메일, 이름, 비밀번호로 회원가입
     func registerUser() {
-        guard let email = emailTextField.text, let password = passwordTextView.text, let name = nameTextField.text else {
+        print("registerUser")
+        guard
+            let email = emailTextField.text,
+            let password = passwordTextView.text,
+            let userName = nameTextField.text else {
             return
         }
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let authResult = authResult {
+                print("registerUser 성공")
+                self.addUserInfoToDB(id: authResult.user.uid, userName: userName)
+            }
             if let error = error {
                 print("SignUp Error: \(error.localizedDescription)")
                 return
@@ -55,13 +94,77 @@ class SignUpViewController: UIViewController {
                 print("signUp 오류")
                 return
             }
-            self.addUserToDB(id: authResult.user.uid, name: name)
         }
     }
         
-    func addUserToDB(id: String, name: String) {
+    func addUserInfoToDB(id: String, userName: String) {
+        print("addUserInfoToDB - id: \(id) userName: \(userName)")
+        guard let image = profileImage else {
+            self.addUserToDB(id: id, userName: userName, imageURL: "")
+            return
+        }
+        let imageData = profileImage.jpegData(compressionQuality: 0.2)!
+        
+        let uploadRef = storage.reference().child("userImages").child(id)
+        let uploadTask = uploadRef.putData(imageData, metadata: nil) { (metadata, error) in
+            if let error = error {
+                print("addProfileImageToDB 오류")
+            }
+        }
+        uploadRef.downloadURL { (url, error) in
+            guard let downloadURL = url else {
+              return
+            }
+            let imageURL = downloadURL.absoluteString
+            self.addUserToDB(id: id, userName: userName, imageURL: imageURL)
+        }
+    }
+    
+    /// 유저 Doc 생성
+    func addUserToDB(id: String, userName: String, imageURL: String) {
+        print("addUserToDB - id: \(id) userName: \(userName) imageURL: \(imageURL)")
         db.collection("users").document(id).setData([
-          "name": name
-        ])
+          "userName": userName,
+          "profileImageURL": imageURL
+        ]){ err in
+            if let err = err {
+              print("Error writing document: \(err)")
+            } else {
+              print("Document successfully written!")
+            } 
+          }
+    }
+    
+}
+
+extension SignUpViewController: PHPickerViewControllerDelegate {
+// MARK: PHPickerViewController
+    
+    func presentPicker() {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+        
+        let imagePicker = PHPickerViewController(configuration: config)
+        imagePicker.delegate = self
+        
+        self.present(imagePicker, animated: true)
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        let itemProviders = results.map(\.itemProvider)
+        DispatchQueue.global().async {
+            for itemProvider in itemProviders {
+                if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                        guard let self = self, let image = image as? UIImage else { return }
+                        self.profileImage = image
+                        self.didFinishPickingDone.onNext(())
+                    }
+                }
+            }
+
+        }
+        picker.dismiss(animated: true)
     }
 }

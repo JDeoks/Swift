7d7c84805d54bb67e4f8687a77c14d88b6f919f2
JDeoks <duginee@naver.com>
2023-12-09 09:59:46 +0900

    Fetures/Chat 0-5강

--- Features/Chat/Chat/ViewControllers/LoginViewController.swift
+++ Features/Chat/Chat/ViewControllers/LoginViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -20,10 +21,18 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        try! Auth.auth().signOut()
         action()
+        bind()
     }
     
     func action() {
+        loginButton.rx.tap
+            .subscribe { _ in
+                self.performLogin()
+            }
+            .disposed(by: disposeBag)
+        
         signInButton.rx.tap
             .subscribe { _ in
                 let signUpVC = self.storyboard?.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
@@ -31,6 +40,35 @@ class LoginViewController: UIViewController {
                 self.present(signUpVC, animated: true)
             }
             .disposed(by: disposeBag)
+    }
+    
+    func bind() {
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user {
+                let mainVC = self.storyboard?.instantiateViewController(identifier: "MainViewController") as! MainViewController
+                mainVC.modalPresentationStyle = .overFullScreen
+                self.present(mainVC, animated: true)
+            }
+        }
+    }
+    
+    func performLogin() {
+        print("LoginViewController - performLogin")
+        guard let email = emailTextField.text, let password = passwordTextView.text else {
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) {authResult, error in
+            if let error = error {
+                print("performLogin 오류\(error)")
+                self.showLoginErrorAlert(title: "로그인 오류", message: error.localizedDescription.debugDescription)
+            }
+        }
+    }
+    
+    func showLoginErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in print("yes 클릭") }))
+        self.present(alert, animated: true)
     }
 
 }

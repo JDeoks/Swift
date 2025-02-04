7fca7c127c1b10d23db3ea0d85411213d06970cb
JDeoks <duginee@naver.com>
2025-02-05 01:42:48 +0900

    RealmTest - OfflineImage 추가

--- Features/RealmTest/RealmTest/OfflineImage.swift
+++ Features/RealmTest/RealmTest/OfflineImage.swift
@@ -9,11 +9,18 @@ import Foundation
 import RealmSwift
 
 class OfflineImage: Object {
+  
+  enum Key {
+    static let id = "id"
+    static let fileName = "fileName"
+    static let width = "width"
+    static let height = "height"
+  }
   @Persisted var id: String?
-  @Persisted var uriStr: String?
+  @Persisted var fileName: String?
   @Persisted var width: Double = 0.0
   @Persisted var height: Double = 0.0
   
   // Core Data에서는 관계의 inverse를 설정했지만, Realm에서는 직접 설정 X
-  @Persisted(originProperty: "beforeImages") var defect: LinkingObjects<OfflineDefect>
+  @Persisted(originProperty: OfflineDefect.Key.beforeImages) var defect: LinkingObjects<OfflineDefect>
 }
